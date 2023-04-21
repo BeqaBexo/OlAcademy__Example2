34edbdc5e0653fca45f0864fec5b3f0d3bc285ea
BeqaBexo <beqacaava1997@gmail.com>
2023-04-21 23:31:57 +0400

    Add project files.

---
+++ OlAcademy__Example2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+/*დავალება 2: შეყვანილი რიცხვის ორობით ფორმატში გადაყვანა*/
+
+Console.Write("-----------------------------------------------------\n");
+Console.Write("-----------------------------------------------------\n");
+Console.Write("-----------------------------------------------------\n");
+Console.Write("Sheiyvanet mnishvneloba :))\n");
+
+string? input = Console.ReadLine();
+
+try
+{
+    if (input != null)
+    {
+        Console.WriteLine($"Sheyvanili mnishvneloba --> {input}");
+
+        int conInput = Convert.ToInt32(input);
+
+
+
+        Console.WriteLine($"Orobit formatshi --> {Convert.ToString(conInput, 2)}");
+    }
+    else
+    {
+        Console.WriteLine("Mnishvleoba ar shegiyvania !!! ");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
